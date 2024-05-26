c3586de6b35995e6d41d22f2a411806c1d82c638
Mateus Picanço <mateus@papodedados.com>
2024-05-26 02:42:28 -0300

    Adding Telegram + Langchain + Maritalk workflow integration

--- alvinbot/alvinbot/bot.py
+++ alvinbot/alvinbot/bot.py
@@ -1,12 +1,32 @@
 from typing import Final
+import json
+import os
+
+# Telegram
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from dotenv import load_dotenv
-import os
+
+# LLM integration
+from langchain_community.chat_models import ChatMaritalk
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts.chat import ChatPromptTemplate
 
 load_dotenv("config.env")
 TELEGRAM_BOT_TOKEN: Final = os.environ.get("TELEGRAM_BOT_TOKEN")
 TELEGRAM_BOT_USERNAME: Final = os.environ.get("TELEGRAM_BOT_USERNAME")
+MARITALK_API_KEY = os.environ.get("MARITALK_API_KEY")
+
+llm = ChatMaritalk(
+    model="sabia-2-small",
+    api_key=MARITALK_API_KEY,  
+    temperature=0.7,
+    max_tokens=100
+)
+
+# read the system message:
+with open('system_prompts.json', 'r') as input_file:
+    system_prompt = json.load(input_file)
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -17,12 +37,29 @@ Olá! Eu sou o Alvin, seu assistente de benfeitorias. O Brasil todo está se esf
 
 # handling responses
 def handle_response(text: str) -> str:
+
     processed: str = text.lower()
 
-    if "alvin" in processed:
-        return "Meu nome é Alvin, e eu sou um assistente de benfeitorias"
+    output_parser = StrOutputParser()
 
-    return "Não entendi o que você perguntou, poderia tentar novamente?"
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                system_prompt["system"],
+            ),
+            (
+                "human", 
+                "Eu tenho {item} para doar"
+            ),
+        ]
+    )
+
+    chain = chat_prompt | llm | output_parser
+
+    response = chain.invoke({"item": processed})
+
+    return response
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
