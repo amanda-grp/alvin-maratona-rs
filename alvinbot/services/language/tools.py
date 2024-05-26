89885484798a8f93de44c66f35c78bdf7111bff6
Mateus Picanço <mateus@papodedados.com>
2024-05-26 18:09:11 -0300

    [feat] adds distance estimation

--- alvinbot/services/language/tools.py
+++ alvinbot/services/language/tools.py
@@ -1,113 +1,230 @@
 import pandas as pd
+from alvinbot.services.location import encode_address, get_haversine_distance
 
 def get_all_available_tools() -> list:
     """
-        Returns a list o tools (such as API and database requests, and other functions) available
-        to the LLM model.
+    Returns a list o tools (such as API and database requests, and other functions) available
+    to the LLM model.
     """
     return [
         BuscarListaDeAbrigosCadastrados,
         BuscarListaDeAbrigosParaMulheresECriancasApenas,
         BuscarAbrigosPrecisandoDeDoacoes,
         BuscarChavesPixCadastradas,
-        VerificarValidadeDeChavePix
+        VerificarValidadeDeChavePix,
     ]
+
 
 ## Buscas relacionadas a abrigos
 def BuscarListaDeAbrigosCadastrados(cidade: str = None) -> str:
     """
-        Retorna entidades ou organizações cadastradas para servirem de abrigos em caso de algum desastre.
-        Parâmetro cidade é opcional.
+    Retorna entidades ou organizações cadastradas para servirem de abrigos em caso de algum desastre.
+    Parâmetro cidade é opcional.
     """
     dfEntities = pd.read_excel("/alvinbot/data/tables/Mock_Entidades.xlsx", header=1)
 
     df = dfEntities[(dfEntities.CadastradoComoAbrigo == True)]
     if cidade:
         df = dfEntities[(dfEntities.Cidade == cidade)]
-    df = df[["Nome", "EndereçoLogradouro", "EndereçoNúmero", "EndereçoComplemento", "CEP", "Bairro", "Cidade", "Estado", "Telefone", "LotacaoAutorizada"]]
+    df = df[
+        [
+            "Nome",
+            "EndereçoLogradouro",
+            "EndereçoNúmero",
+            "EndereçoComplemento",
+            "CEP",
+            "Bairro",
+            "Cidade",
+            "Estado",
+            "Telefone",
+            "LotacaoAutorizada",
+        ]
+    ]
 
     return df.to_json()
 
+
 def BuscarListaDeAbrigosParaMulheresECriancasApenas(cidade: str = None) -> str:
     """
-        Retorna abrigos que acolhem apenas mulheres e crianças.
-        Parâmetro cidade é opcional.
+    Retorna abrigos que acolhem apenas mulheres e crianças.
+    Parâmetro cidade é opcional.
     """
-    dfOccurences = pd.read_excel("/alvinbot/data/tables/Mock_Ocorrencias.xlsx", header=1)
+    dfOccurences = pd.read_excel(
+        "/alvinbot/data/tables/Mock_Ocorrencias.xlsx", header=1
+    )
     dfEntities = pd.read_excel("/alvinbot/data/tables/Mock_Entidades.xlsx", header=1)
     dfShelters = pd.read_excel("/alvinbot/data/tables/Mock_Abrigos.xlsx", header=1)
 
     df = dfOccurences[dfOccurences["DataFim"].isna()]
-    df = df.merge(dfShelters[(dfShelters["GruposRestritos"] == "Apenas Mulheres e Crianças") & (dfShelters["DataTermino"].isna())], how='inner', left_on=['ID'],right_on=['IDOcorrencia'])
-    df = df.merge(dfEntities, how='inner', left_on=['IDEntidade'],right_on=['ID'])
+    df = df.merge(
+        dfShelters[
+            (dfShelters["GruposRestritos"] == "Apenas Mulheres e Crianças")
+            & (dfShelters["DataTermino"].isna())
+        ],
+        how="inner",
+        left_on=["ID"],
+        right_on=["IDOcorrencia"],
+    )
+    df = df.merge(dfEntities, how="inner", left_on=["IDEntidade"], right_on=["ID"])
 
-    df = df[["Nome_y", "EndereçoLogradouro", "EndereçoNúmero", "EndereçoComplemento", "CEP", "Bairro", "Cidade", "Estado", "Telefone", "LotacaoAutorizada", "GruposRestritos"]]
+    df = df[
+        [
+            "Nome_y",
+            "EndereçoLogradouro",
+            "EndereçoNúmero",
+            "EndereçoComplemento",
+            "CEP",
+            "Bairro",
+            "Cidade",
+            "Estado",
+            "Telefone",
+            "LotacaoAutorizada",
+            "GruposRestritos",
+        ]
+    ]
 
     return df.to_json()
+
 
 ## Buscas relacionadas a doações e voluntariado
 def BuscarAbrigosPrecisandoDeDoacoes(cidade: str = None, item: str = None) -> str:
     """
-        Retorna entidades ou organizações precisando de doações, quais items, urgência e quantidade.
-        Parâmetros cidade e item são opcionais. O item deve ser informado no singular.
+    Retorna entidades ou organizações precisando de doações, quais items, urgência e quantidade.
+    Parâmetros cidade e item são opcionais. O item deve ser informado no singular.
     """
-    dfOccurences = pd.read_excel("/alvinbot/data/tables/Mock_Ocorrencias.xlsx", header=1)
+    dfOccurences = pd.read_excel(
+        "/alvinbot/data/tables/Mock_Ocorrencias.xlsx", header=1
+    )
     dfEntities = pd.read_excel("/alvinbot/data/tables/Mock_Entidades.xlsx", header=1)
     dfShelters = pd.read_excel("/alvinbot/data/tables/Mock_Abrigos.xlsx", header=1)
-    dfDonationAsks = pd.read_excel("/alvinbot/data/tables/Mock_RequisicaoDeDoacoes.xlsx", header=1)
+    dfDonationAsks = pd.read_excel(
+        "/alvinbot/data/tables/Mock_RequisicaoDeDoacoes.xlsx", header=1
+    )
 
     df = dfOccurences[dfOccurences["DataFim"].isna()]
-    df = df.merge(dfShelters[(dfShelters["DataTermino"].isna())], how='inner', left_on=['ID'],right_on=['IDOcorrencia'])
-    df = df.merge(dfEntities, how='inner', left_on=['IDEntidade'],right_on=['ID'])
+    df = df.merge(
+        dfShelters[(dfShelters["DataTermino"].isna())],
+        how="inner",
+        left_on=["ID"],
+        right_on=["IDOcorrencia"],
+    )
+    df = df.merge(dfEntities, how="inner", left_on=["IDEntidade"], right_on=["ID"])
+    
     if cidade:
         df = df[(df["Cidade"] == cidade)]
 
     df2 = dfDonationAsks[dfDonationAsks["DataRecebido"].isna()]
-    df2 = df2.merge(df, how='inner', left_on=['IDAbrigo'],right_on=['ID_y'])
+    df2 = df2.merge(df, how="inner", left_on=["IDAbrigo"], right_on=["ID_y"])
+
     if item:
         df2 = df2[(df2["Item"].str.contains(item))]
-    df2 = df2[["Nome_y", "EndereçoLogradouro", "EndereçoNúmero", "EndereçoComplemento", "CEP", "Bairro", "Cidade", "Estado", "Telefone", "LotacaoAutorizada", "Categoria", "Item", "Quantidade", "Urgência"]]
+
+    df2 = df2[
+        [
+            "Nome_y",
+            "EndereçoLogradouro",
+            "EndereçoNúmero",
+            "EndereçoComplemento",
+            "CEP",
+            "Bairro",
+            "Cidade",
+            "Estado",
+            "Telefone",
+            "LotacaoAutorizada",
+            "Categoria",
+            "Item",
+            "Quantidade",
+            "Urgência",
+        ]
+    ]
 
     return df2.to_json()
+
 
 ## Buscas relacionadas a chaves PIX cadastradas pelas entidades
 def BuscarChavesPixCadastradas(cidade: str = None) -> str:
     """
-        Retorna chaves pix válidas de entidades ou organizações cadastradas para receberem doações em dinheiro.
-        Parâmetro cidade é opcional.
+    Retorna chaves pix válidas de entidades ou organizações cadastradas para receberem doações em dinheiro.
+    Parâmetro cidade é opcional.
     """
-    dfOccurences = pd.read_excel("/alvinbot/data/tables/Mock_Ocorrencias.xlsx", header=1)
+    dfOccurences = pd.read_excel(
+        "/alvinbot/data/tables/Mock_Ocorrencias.xlsx", header=1
+    )
     dfEntities = pd.read_excel("/alvinbot/data/tables/Mock_Entidades.xlsx", header=1)
     dfShelters = pd.read_excel("/alvinbot/data/tables/Mock_Abrigos.xlsx", header=1)
-    dfDonationCenters = pd.read_excel("/alvinbot/data/tables/Mock_CentroDeDoacoes.xlsx", header=1)
+    dfDonationCenters = pd.read_excel(
+        "/alvinbot/data/tables/Mock_CentroDeDoacoes.xlsx", header=1
+    )
 
     df = dfOccurences[dfOccurences["DataFim"].isna()]
-    dfShelters = df.merge(dfShelters[(dfShelters["DataTermino"].isna())], how='inner', left_on=['ID'],right_on=['IDOcorrencia'])[["IDEntidade", "ChavePix"]]
-    dfDonationCenters = df.merge(dfDonationCenters[(dfDonationCenters["DataTermino"].isna())], how='inner', left_on=['ID'],right_on=['IDOcorrencia'])[["IDEntidade", "ChavePix"]]
+    dfShelters = df.merge(
+        dfShelters[(dfShelters["DataTermino"].isna())],
+        how="inner",
+        left_on=["ID"],
+        right_on=["IDOcorrencia"],
+    )[["IDEntidade", "ChavePix"]]
+    dfDonationCenters = df.merge(
+        dfDonationCenters[(dfDonationCenters["DataTermino"].isna())],
+        how="inner",
+        left_on=["ID"],
+        right_on=["IDOcorrencia"],
+    )[["IDEntidade", "ChavePix"]]
 
     df = pd.concat([dfShelters, dfDonationCenters])
-    df = (df[~df["ChavePix"].isna()]).merge(dfEntities, how='inner', left_on=['IDEntidade'],right_on=['ID'])[["Nome", "ChavePix", "EndereçoLogradouro", "EndereçoNúmero", "EndereçoComplemento", "CEP", "Bairro", "Cidade", "Estado", "Telefone", "AreaDeAtuacao"]]
+    df = (df[~df["ChavePix"].isna()]).merge(
+        dfEntities, how="inner", left_on=["IDEntidade"], right_on=["ID"]
+    )[
+        [
+            "Nome",
+            "ChavePix",
+            "EndereçoLogradouro",
+            "EndereçoNúmero",
+            "EndereçoComplemento",
+            "CEP",
+            "Bairro",
+            "Cidade",
+            "Estado",
+            "Telefone",
+            "AreaDeAtuacao",
+        ]
+    ]
 
     if cidade:
         df = df[(df["Cidade"] == cidade)]
 
     return df.to_json()
 
+
 def VerificarValidadeDeChavePix(chavePix: str) -> str:
-  """
+    """
     Verifica se a chave pix informada pelo usuário existe e qual a entidade que a cadastrou.
-  """
-  dfOccurences = pd.read_excel("/alvinbot/data/tables/Mock_Ocorrencias.xlsx", header=1)
-  dfEntities = pd.read_excel("/alvinbot/data/tables/Mock_Entidades.xlsx", header=1)
-  dfShelters = pd.read_excel("/alvinbot/data/tables/Mock_Abrigos.xlsx", header=1)
-  dfDonationCenters = pd.read_excel("/alvinbot/data/tables/Mock_CentroDeDoacoes.xlsx", header=1)
+    """
+    dfOccurences = pd.read_excel(
+        "/alvinbot/data/tables/Mock_Ocorrencias.xlsx", header=1
+    )
+    dfEntities = pd.read_excel("/alvinbot/data/tables/Mock_Entidades.xlsx", header=1)
+    dfShelters = pd.read_excel("/alvinbot/data/tables/Mock_Abrigos.xlsx", header=1)
+    dfDonationCenters = pd.read_excel(
+        "/alvinbot/data/tables/Mock_CentroDeDoacoes.xlsx", header=1
+    )
 
-  df = dfOccurences[dfOccurences["DataFim"].isna()]
-  dfShelters = df.merge(dfShelters[(dfShelters["DataTermino"].isna())], how='inner', left_on=['ID'],right_on=['IDOcorrencia'])[["IDEntidade", "ChavePix"]]
-  dfDonationCenters = df.merge(dfDonationCenters[(dfDonationCenters["DataTermino"].isna())], how='inner', left_on=['ID'],right_on=['IDOcorrencia'])[["IDEntidade", "ChavePix"]]
+    df = dfOccurences[dfOccurences["DataFim"].isna()]
 
-  df = pd.concat([dfShelters, dfDonationCenters])
-  df = df[(df["ChavePix"] == chavePix)]
+    dfShelters = df.merge(
+        dfShelters[(dfShelters["DataTermino"].isna())],
+        how="inner",
+        left_on=["ID"],
+        right_on=["IDOcorrencia"],
+    )[["IDEntidade", "ChavePix"]]
 
-  return df.to_json() or "Chave pix não encontrada no banco de dados oficial"
+    dfDonationCenters = df.merge(
+        dfDonationCenters[(dfDonationCenters["DataTermino"].isna())],
+        how="inner",
+        left_on=["ID"],
+        right_on=["IDOcorrencia"],
+    )[["IDEntidade", "ChavePix"]]
 
+    df = pd.concat([dfShelters, dfDonationCenters])
+    df = df[(df["ChavePix"] == chavePix)]
+
+    return df.to_json() or "Chave pix não encontrada no banco de dados oficial"
--- alvinbot/services/location/location.py
+++ alvinbot/services/location/location.py
@@ -1,13 +1,41 @@
 import os
 from datetime import datetime
 from dotenv import load_dotenv
-from typing import Union, Iterable
+from typing import Union, Iterable, NewType, Tuple
+from math import radians, cos, sin, asin, sqrt
 
 # Maps dependencies
 import googlemaps
 
 load_dotenv("config.env")
 gmaps_client = googlemaps.Client(key=os.environ.get("MAPS_API_KEY"))
+
+EARTH_RADIUS_KM = 6372.8 # Radius of earth in kilometers. Use 3956 for miles.
+
+# Auxiliary type for Coordinate representation
+Coordinate = NewType(
+    name = "Coordinates",
+    tp = Tuple[float, float]
+)
+
+def get_haversine_distance(origin_coord_pair: Coordinate, target_coord_pair: Coordinate) -> float:
+    """
+    Calculate the great circle distance in kilometers between two points 
+    on the earth (specified in decimal degrees)
+    """
+
+    origin_lat, origin_lon = origin_coord_pair
+    target_lat, target_lon = target_coord_pair
+
+    # Decimal degrees to Radians conversion
+    origin_lon, origin_lat, target_lon, target_lat = map(radians, [origin_lon, origin_lat, target_lon, target_lat])
+
+    # Haversine formula calculation
+    dlon = target_lon - origin_lon 
+    dlat = target_lat - origin_lat 
+    a = sin(dlat/2)**2 + cos(origin_lat) * cos(target_lat) * sin(dlon/2)**2
+    conversion_constant = 2 * asin(sqrt(a)) 
+    return conversion_constant * EARTH_RADIUS_KM
 
 def encode_address(address: Union[str, Iterable], is_result: bool = False):
     """Turns an address in plain text into a Latitude, Longitude tuple pair
@@ -30,7 +58,7 @@ def encode_address(address: Union[str, Iterable], is_result: bool = False):
         (latitude, longitude) = (geometry_data["lat"], geometry_data["lng"])
 
     else:
-        
+
         result = gmaps_client.geocode(
             address
         )
@@ -39,4 +67,4 @@ def encode_address(address: Union[str, Iterable], is_result: bool = False):
 
         (latitude, longitude) = (geometry_data["lat"], geometry_data["lng"])
 
-    return (latitude, longitude)
+    return Coordinate((latitude, longitude))
