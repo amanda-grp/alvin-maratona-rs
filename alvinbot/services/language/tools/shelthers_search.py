40c46c0cf59a3b3a1f09bbb15df0a08da56222cf
Damares Resende <doliveirader@microsoft.com>
2024-05-26 21:34:16 -0300

    Fixing path in tools for real

--- alvinbot/services/language/tools/shelthers_search.py
+++ alvinbot/services/language/tools/shelthers_search.py
@@ -8,7 +8,7 @@ def BuscarListaDeAbrigosCadastrados(cidade: str = None) -> str:
         Retorna entidades ou organizações cadastradas para servirem de abrigos em caso de algum desastre.
         Parâmetro cidade é opcional.
     """
-    dfEntities = pd.read_excel("/alvinbot/data/tables/Mock_Entidades.xlsx".replace('/', os.sep()), header=1)
+    dfEntities = pd.read_excel("alvinbot/data/tables/Mock_Entidades.xlsx".replace('/', os.sep), header=1)
 
     df = dfEntities[(dfEntities.CadastradoComoAbrigo == True)]
     if cidade:
@@ -23,9 +23,9 @@ def BuscarListaDeAbrigosParaMulheresECriancasApenas(cidade: str = None) -> str:
         Retorna abrigos que acolhem apenas mulheres e crianças.
         Parâmetro cidade é opcional.
     """
-    dfOccurences = pd.read_excel("/alvinbot/data/tables/Mock_Ocorrencias.xlsx".replace('/', os.sep()), header=1)
-    dfEntities = pd.read_excel("/alvinbot/data/tables/Mock_Entidades.xlsx".replace('/', os.sep()), header=1)
-    dfShelters = pd.read_excel("/alvinbot/data/tables/Mock_Abrigos.xlsx".replace('/', os.sep()), header=1)
+    dfOccurences = pd.read_excel("alvinbot/data/tables/Mock_Ocorrencias.xlsx".replace('/', os.sep), header=1)
+    dfEntities = pd.read_excel("alvinbot/data/tables/Mock_Entidades.xlsx".replace('/', os.sep), header=1)
+    dfShelters = pd.read_excel("alvinbot/data/tables/Mock_Abrigos.xlsx".replace('/', os.sep), header=1)
 
     df = dfOccurences[dfOccurences["DataFim"].isna()]
     df = df.merge(dfShelters[(dfShelters["GruposRestritos"] == "Apenas Mulheres e Crianças") & (dfShelters["DataTermino"].isna())], how='inner', left_on=['ID'],right_on=['IDOcorrencia'])
