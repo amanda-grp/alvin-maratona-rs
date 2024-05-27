6029ecb89442d92fea4624c62a92a6f53cd2da1f
Amanda Goldani <62963404+amanda-grp@users.noreply.github.com>
2024-05-26 23:17:49 -0300

    Langchain (#12)
    * langchain
    
    * Drop alvinbot from path
    
    * use langchain
    
    ---------
    
    Co-authored-by: Amanda G. R. P <amanda.goldani@hotmail.com>

--- alvinbot/services/language/llm.py
+++ alvinbot/services/language/llm.py
@@ -1,61 +1,106 @@
 import os
+import json
+from typing import Final
 from dotenv import load_dotenv
-import google.generativeai as genai
 
-from alvinbot.services.language.tools.tools import get_all_available_tools
-from alvinbot.services.common.templater import load_template_file
+from services.language.tools.tools import get_all_available_tools, get_tool_map
+from services.common.templater import load_template_file
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable, RunnablePassthrough
+
 
 load_dotenv("config.env")
-genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
+GOOGLE_API_KEY: Final = os.environ.get("GOOGLE_API_KEY")
+GOOGLE_PROJECT_ID: Final = os.environ.get("GOOGLE_PROJECT_ID")
 
 def get_system_instructions() -> str:
     """
         Returns the system instructions that define how Alvin behaves when interacting with the user.
     """
-    prompts = load_template_file(
-        'alvinbot/templates/prompts.yaml'
-    )
+    prompts = load_template_file('templates/prompts.yaml')
+    return prompts["prompts"]["system"] + prompts["prompts"]["context"]
 
-    return prompts["prompts"]["system"] + prompts["prompts"]["context"],
+def get_standard_hello_message() -> str:
+    commands = load_template_file('templates/commands.yaml')
 
-def start_model(enable_tools:bool = True) -> genai.GenerativeModel:
+    return f"Se apresente apenas uma vez e com base na mensagem: \"{commands["commands"]["start"]}\". Seja breve, claro e não se esqueça de falar sobre a funcionalidade do SOS"
+
+
+def get_system_prompt_template(with_hello_message: bool = False) -> ChatPromptTemplate:
+    if with_hello_message:
+        return ChatPromptTemplate.from_messages(
+            [
+                ("system", get_system_instructions()),
+                ("human", "{hello_message}")
+            ]
+        )
+    return ChatPromptTemplate.from_messages([("system", get_system_instructions()), ("human", "")])
+
+def say_hello(chat_session: ChatGoogleGenerativeAI) -> str:
+    prompt = get_system_prompt_template(with_hello_message=True)
+    chain = prompt | chat_session | parse_raw_output
+    output = chain.invoke({"hello_message": get_standard_hello_message()})
+
+    return parse_content_response(output)
+
+
+def start_chat_session(model: str, enable_tools: bool = True) -> ChatGoogleGenerativeAI:
     """
-        Returns the generative model based on the system instructions and available tools.
+        Returns the chat started from the model.
     """
-    tools = get_all_available_tools() if enable_tools else []
+    llm = ChatGoogleGenerativeAI(model=model, google_api_key=GOOGLE_API_KEY)
+    if enable_tools:
+        llm = llm.bind_tools(get_all_available_tools())
 
-    return genai.GenerativeModel(
-        model_name="gemini-1.5-flash",
-        system_instruction=get_system_instructions(),
-        tools=tools
-    )
+    return llm
 
-def start_chat_session(model: genai.GenerativeModel) -> genai.ChatSession:
-   """
-    Returns the chat started from the model.
-   """
-   return model.start_chat(history=[], enable_automatic_function_calling=True)
-
-def get_response_to_user_message(user_message: str, chat_session: genai.ChatSession) -> str:
+def get_response_to_user_message(user_message: str, chat_session: ChatGoogleGenerativeAI) -> str:
     """
         Returns response to user messsage
     """
-    response = chat_session.send_message(user_message, stream=False)
-    return parse_response(response)
+    #prompt = get_system_prompt_template()
+    #chain = prompt | chat_session | parse_raw_output
+    chain = chat_session | parse_raw_output
+    output = chain.invoke(user_message)
 
-def parse_response(response: list) -> str:
-  """
-    Parses the model response and return the full string message.
-  """
-  full_response = ""
-  for chunk in response:
-    full_response += chunk.text
+    if output.tool_calls != []:
+        return parse_tool_response(user_message, output, chat_session)
 
-  return full_response
+    return parse_content_response(output)
+
+def parse_raw_output(response: AIMessage) -> Runnable:
+    tool_calls = response.tool_calls.copy()
+    for tool_call in tool_calls:
+        tool_call["output"] = get_tool_map()[tool_call["name"]].invoke(tool_call["args"])
+    return response
+
+def parse_content_response(response: AIMessage) -> str:
+    """
+        Parses the model response and return the full string message.
+    """
+    return response.content or "Ah não! Desculpa não consegui obter nenhuma informação :("
+
+def parse_tool_response(user_message: str, response: AIMessage, chat_session: ChatGoogleGenerativeAI) -> str:
+    """
+        Parses the model response and return the full string message.
+    """
+    analyze_tool_response = ChatPromptTemplate.from_template(
+        "Responda a messagem do usuário: {message}, com base no output: {tool_call}. Não mencione nada sobre a função em si"
+    )
+
+    chain = {"tool_call": RunnablePassthrough(), "message": RunnablePassthrough()} | analyze_tool_response | chat_session
+    analyzed_response = chain.invoke(
+        {"tool_call": f"{json.dumps(response.tool_calls)}", "message": f"{user_message}"}
+    )
+    
+    return parse_content_response(analyzed_response)
 
 if __name__ == "__main__":
-    model = start_model()
-    chat = start_chat_session(model)
+    chat = start_chat_session(model="gemini-1.5-flash")
+    print(say_hello(chat))
 
     while True:
         prompt = input("Pergunte alguma coisa: ")
--- alvinbot/services/language/tools/tools.py
+++ alvinbot/services/language/tools/tools.py
@@ -1,6 +1,6 @@
-from alvinbot.services.language.tools.donations_search import BuscarAbrigosPrecisandoDeDoacoes, BuscarChavesPixCadastradas, VerificarValidadeDeChavePix
-from alvinbot.services.language.tools.shelthers_search import BuscarListaDeAbrigosCadastrados, BuscarListaDeAbrigosParaMulheresECriancasApenas
-from alvinbot.services.language.tools.alerts_search import BuscarAlertasDePerigoParaAPopulacaoAtuais, BuscarAlertasDePerigoParaAPopulacaoExpirados
+from services.language.tools.donations_search import BuscarAbrigosPrecisandoDeDoacoes, BuscarChavesPixCadastradas, VerificarValidadeDeChavePix
+from services.language.tools.shelthers_search import BuscarListaDeAbrigosCadastrados, BuscarListaDeAbrigosParaMulheresECriancasApenas
+from services.language.tools.alerts_search import BuscarAlertasDePerigoParaAPopulacaoAtuais, BuscarAlertasDePerigoParaAPopulacaoExpirados
 
 def get_all_available_tools() -> list:
     """
@@ -16,3 +16,6 @@ def get_all_available_tools() -> list:
         BuscarAlertasDePerigoParaAPopulacaoAtuais,
         BuscarAlertasDePerigoParaAPopulacaoExpirados
     ]
+
+def get_tool_map() -> dict:
+    return {tool.name: tool for tool in get_all_available_tools()}
